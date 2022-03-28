47539a5bb0709014a8e36f9919e30d30f161672e
katiekeegan <66965763+katiekeegan@users.noreply.github.com>
2022-03-28 13:51:10 -0400

    Update test_GraphCut.py

--- test_GraphCut.py
+++ test_GraphCut.py
@@ -9,13 +9,15 @@ Created on Thu Mar 10 17:51:13 2022
 import match
 import cv2
 
-
 # main programme
 def main(file1, file2, color, dispMin, dispMax):
     # load two images
     imgLeft = cv2.imread(file1)
     imgRight = cv2.imread(file2)
-
+    #imgLeft = imgLeft[200:, 100:]
+    #imgLeft = cv2.resize(imgLeft, (200,300) ,interpolation = cv2.INTER_AREA)
+    #imgRight = cv2.resize(imgRight, (200,300) ,interpolation = cv2.INTER_AREA)
+    
     # Default parameters
     K = -1
     lambda_ = -1
@@ -27,11 +29,18 @@ def main(file1, file2, color, dispMin, dispMax):
                               lambda1=lambda1,
                               lambda2=lambda2,
                               K=K,
-                              maxIter=4)
+                              maxIter=10,
+                              sigma_d = 14,
+                              sigma_s = 3.8,
+                              lambda_ancc = 1/30,
+                              V_max = 16,
+                              width = 3,
+                              theta = 0.7,
+                              gamma = 1
+                              )
 
     # create match instance
     m = match.Match(imgLeft, imgRight, color)
-
     m.SetDispRange(dispMin, dispMax)
     m = match.fix_parameters(m, params, K, lambda_, lambda1, lambda2)
     m.kolmogorov_zabih()
@@ -39,9 +48,10 @@ def main(file1, file2, color, dispMin, dispMax):
 
 
 if __name__ == '__main__':
-    filename_left = "./images/imgR1.png"
-    filename_right = "./images/imgL1.png"
+    filename_left = "./images/perra_7.jpg"
+    filename_right = "./images/perra_8.jpg"
     is_color = True
     disMin = -16
     disMax = 16
     main(filename_left, filename_right, is_color, disMin, disMax)
+    
